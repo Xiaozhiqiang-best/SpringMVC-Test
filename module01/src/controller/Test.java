8547f1e426a627728580070c7cfd28f712af785a
xiaozhiqiang <2064914229@qq.com>
2021-01-19 16:11:49 +0800

    Initial commit

---
+++ module01/src/controller/Test.java
@@ -0,0 +1,12 @@
+package controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Map<String, String> map = new HashMap<>();
+
+    }
+}
